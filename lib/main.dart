b10be6ee0b47776d51e8414ff4c8144013ab5e34
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-11-19 17:27:54 +0100

    tile map

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,15 +11,22 @@ class LeenaGame extends FlameGame {
   SpriteComponent leena = SpriteComponent();
   double gravity = 1.8;
   Vector2 velocity = Vector2(0, 0); //*velocità 2d
+  late TiledComponent homeMap;
 
   @override
   Future<void>? onLoad() async {
     // TODO: implement onLoad
     await super.onLoad();
+    //*TILES
+    homeMap = await TiledComponent.load('map.tmx', Vector2.all(32));
     leena
       ..sprite = await loadSprite('girl.png')
       ..size = Vector2.all(100)
       ..position = Vector2(100, 30);
+    // add(SpriteComponent()
+    //   ..sprite = await loadSprite('background.png')
+    //   ..size = size);
+    add(homeMap);
     add(leena);
   }
 
